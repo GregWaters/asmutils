9e5f869241738cee9705e52eaa1768b7439df2ab
GregWaters <gregoryarthurwaters@gmail.com>
2024-05-14 11:58:18 -0500

    Update clear

---
+++ src/clear.asm
@@ -0,0 +1,29 @@
+%include "macros.asm"
+
+section .data
+	clear: db `\ec`
+
+section .text
+global _start
+
+	_start:
+
+%if BITS == 32
+	; clear the active terminal screen
+	mov eax, 4     ; 'write' syscall number (x86)
+	mov edi, 1     ; stdout
+	mov ecx, clear ; 'clear' escape sequence
+	mov edx, 2     ; length of string
+	syscall
+	
+%elif BITS == 64
+
+	mov rax, 1     ; 'write' syscall number (x64)
+	mov rdi, 1     ; stdout
+	mov rsi, clear ; 'clear' escape sequence
+	mov rdx, 2     ; length of string
+	syscall
+
+%endif
+
+	exit 0
