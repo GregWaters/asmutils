99b97f46192a47dd8728a735e5119b0cefbec407
GregWaters <gregoryarthurwaters@gmail.com>
2024-05-15 14:14:58 -0500

    Refactor + add top comment

--- src/clear.asm
+++ src/clear.asm
@@ -1,3 +1,5 @@
+; clear - clear the terminal screen
+
 %include "macros.asm"
 
 section .data
@@ -9,20 +11,14 @@ global _start
 	_start:
 
 %if BITS == 32
-	; clear the active terminal screen
 	mov eax, 4     ; 'write' syscall number (x86)
-	mov edi, 1     ; stdout
-	mov ecx, clear ; 'clear' escape sequence
-	mov edx, 2     ; length of string
-	
 %elif BITS == 64
-
 	mov rax, 1     ; 'write' syscall number (x64)
-	mov rdi, 1     ; stdout
-	mov rsi, clear ; 'clear' escape sequence
-	mov rdx, 2     ; length of string
-
 %endif
 
+	mov arg0, 1     ; stdout
+	mov arg1, clear ; 'clear' escape sequence
+	mov arg2, 2     ; length of string
+	
 	syscall ; call 'write' with the given parameters
 	exit 0 ; exit the program
