2ec65b9d731dc62d9175b0296f93b082f67643b9
Greg Waters <gregoryarthurwaters@gmail.com>
2024-05-30 16:16:50 -0500

    Make absolutely sure cat is functional when called

--- src/cat.asm
+++ src/cat.asm
@@ -1,35 +1,49 @@
 ;==============================================================================
 ; cat - concatenate files and print on the standard output
-; /!\ Work in progress /!\ Later fix sendfile call!
+;
+; TODO - Work with stack operations and move past 'testing'
+; with a static filename variable
 ;==============================================================================
 
 section .text
 global _start
 
 _start:
-    mov rax, 2
-    mov rdi, filename
-    mov rdx, 0o755 ; standard permissions -- may be incorrect
+    ; basically being a prick and lying so the program works
+    mov eax, 72
+    inc edi
+    mov esi, 4
+    syscall ; fcntl(1, 4, O_RDONLY)
+
+    pop rcx ; get argc (TODO)
+
+.loop_begin: ; TODO
+    ; get file descriptor
+    mov al, 2
+    mov edi, filename
+    xor esi, esi
     syscall ; open(filename)
 
-    mov rdi, rax
-    mov rax, 5
-    mov rsi, statbuf
-    syscall
+    ; get file size
+    mov edi, eax
+    mov eax, 5
+    mov esi, statbuf
+    syscall ; fstat(fd, &statbuf)
 
-    mov rax, 40
-    mov rsi, rdi
-    mov rdi, 1
-    mov rdx, 0 ; interpreted as nullptr
+    ; use sendfile to output the entire file
+    mov al, 40
+    mov esi, edi
+    mov edi, 1
+    xor edx, edx ; interpreted as nullptr
     mov r10, [statbuf + 48] ; statbuf.st_size
-    syscall
+    syscall ; sendfile(fd, stdout, NULL, statbuf.st_size)
 
-    mov rax, 60
-    dec rdi
-    syscall
+    mov eax, 60 ; must use eax here, as bits beyond 16 may be overwritten
+    dec edi
+    syscall ; exit(0)
 
 section .bss
     statbuf resb 144 ; reserve 144 bytes for the 64-bit stat structure
 
 section .data
-filename: db "example.s", 0
+filename: db "example.asm", 0
